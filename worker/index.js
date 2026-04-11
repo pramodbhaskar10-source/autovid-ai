22a4a5a8d0b09a0cfab81e2bdc681605be405a00
pramodbhaskar10-source <pramodbhaskar10@gmail.com>
2026-04-11 17:17:58 +0530

    Update index.js

--- worker/index.js
+++ worker/index.js
@@ -1,7 +1,10 @@
-import axios from "axios"
-import express from "express"
+import express from "express" 
+import axios from "axios" 
+import cors from "cors"
+
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 // Health check
